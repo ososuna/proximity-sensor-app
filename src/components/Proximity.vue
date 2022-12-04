1716a71ccf916c22ee543d962d6708e2c227200e
oswi <oswaldo.adrian@live.com.mx>
2022-12-04 16:52:11 -0600

    ui components

---
+++ src/components/Proximity.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import proximityApi from '../api/proximityApi';
+const proximity = ref(0);
+const getSensorData = () => {
+  setInterval( async() => {
+    const { data } = await proximityApi.get('');
+    proximity.value = data;
+    console.log( data );
+  } , 1000);
+};
+getSensorData();
+</script>
+
+<template>
+<div class="container text-light">
+  <div class="row">
+    <div class="col-12">
+      <h1>{{ proximity }} cm</h1>
+    </div>
+  </div>
+</div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import NavbarComponent from '../components/NavbarComponent.vue';
+import Proximity from '../components/Proximity.vue'
+</script>
+<template>
+<NavbarComponent />
+<div class="vh-100 bg-dark">
+  <div class="row d-flex justify-content-center align-items-center h-100">
+    <div class="col-6">
+      <img src="../assets/logo.png" alt="logo">
+    </div>
+    <div class="col-6 d-flex align-items-center justify-content-center">
+      <Proximity />
+    </div>
+  </div>
+</div>
+</template>
